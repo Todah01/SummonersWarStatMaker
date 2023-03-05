69f7e3eae726355399e675ce6047afb29d7c4802
Todah01 <57752105+Todah01@users.noreply.github.com>
2023-03-05 18:01:15 +0900

    2023.03.05
    img maker setting

---
+++ Assets/Script/Exporter/ExporterManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExporterManager : MonoBehaviour
+{
+    public int width = 1024;
+    public int height = 1024;
+    public Camera captureCamera;
+    public Texture2D resultTexture;
+    public Image curImg;
+    public Image resultImage;
+    public string curMonsterName;
+    public string totalMonstersName;
+    public string resultSpriteSavePath;
+    public Sprite[] monster_imgs;
+    public GameObject inputField;
+    public GameObject resultImgViewer;
+    
+    Sprite mySprite;
+
+    public void imgSelector(Image img)
+    {
+        curImg = img;
+    }
+
+    public void SetImg()
+    {
+        curMonsterName = inputField.GetComponent<search_inputfield_control>().CheckMonsterName;
+        // Set monster profile img.
+        string moneter_name_tolower = curMonsterName.ToLower();
+        for (int i = 0; i < monster_imgs.Length; i++)
+        {
+
+            if (monster_imgs[i].name.ToLower().Contains(moneter_name_tolower))
+            {
+                Debug.Log(moneter_name_tolower);
+                curImg.sprite = monster_imgs[i];
+                break;
+            }
+        }
+    }
+
+    public void ExportImg()
+    {
+        // Create a new RenderTexture with the desired dimensions
+        RenderTexture rt = new RenderTexture(width, height, 24);
+
+        // Set the capture camera's target texture to be the new RenderTexture
+        captureCamera.targetTexture = rt;
+
+        // Render the scene to the new RenderTexture
+        captureCamera.Render();
+
+        // Activate the RenderTexture
+        RenderTexture.active = rt;
+
+        // Create a new Texture2D and read the pixels from the RenderTexture into it
+        Texture2D texture = new Texture2D(width, height, TextureFormat.RGB24, false);
+        texture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+
+        // Apply the changes to the Texture2D
+        texture.Apply();
+
+        // Set the resultTexture to the new Texture2D
+        resultTexture = texture;
+
+        // Clean up
+        RenderTexture.active = null;
+        captureCamera.targetTexture = null;
+
+        FooBar(texture);
+
+        resultImgViewer.SetActive(true);
+    }
+
+    public void ExportSprite()
+    {
+        ExportTexture2D(resultTexture, resultSpriteSavePath);
+        Debug.Log("Download Complete!");
+    }
+
+    void ExportTexture2D(Texture2D tex, string outputPath)
+    {
+        byte[] bytes = tex.EncodeToPNG();
+        System.IO.File.WriteAllBytes(outputPath, bytes);
+    }
+
+    public void FooBar(Texture2D myTexture2D)
+    {
+        // Texture2D 객체를 Sprite 객체로 변환
+        mySprite = Sprite.Create(myTexture2D, new Rect(0.0f, 0.0f, myTexture2D.width, myTexture2D.height), new Vector2(0.5f, 0.5f));
+
+        // Sprite를 Image에 적용하여 화면에 표시
+        resultImage.sprite = mySprite;
+    }
+}
---
+++ Assets/Script/Main/SelectUI/Monster_Select/search_inputfield_control_exporter.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class search_inputfield_control_exporter : MonoBehaviour
+{
+    #region Public Variable
+    public GameObject ContentView;
+    public GameObject ContentWindow;
+    public RectTransform ItemParent;
+    public RectTransform ItemPrefab;
+    public string CheckMonsterName;
+    public float levenshteinDistance;
+    #endregion
+
+    public void OnInputValueChanged()
+    {
+        // If user click search field, search result content view will set visible.
+        ContentView.SetActive(true);
+
+        // Reset search result content view.
+        ClearResults();
+
+        // Fill search result in search result content view.
+        FillResults(GetResults(this.GetComponent<InputField>().text));
+    }
+    private void SendMonster(string name)
+    {
+        string monster_name_remove_blank = "";
+        for (int idx = 0; idx < name.Length; idx++)
+        {
+            if (name[idx + 1] == '(')
+                break;
+
+            monster_name_remove_blank += name[idx];
+        }
+
+        this.GetComponent<InputField>().text = monster_name_remove_blank;
+        CheckMonsterName = monster_name_remove_blank;
+        ContentView.SetActive(false);
+    }
+    private void ClearResults()
+    {
+        // Reverse loop since you destroy children
+        for (int childIndex = ItemParent.childCount - 1; childIndex >= 0; --childIndex)
+        {
+            Transform child = ItemParent.GetChild(childIndex);
+            child.SetParent(null);
+            Destroy(child.gameObject);
+        }
+    }
+    private void FillResults(List<string> results)
+    {
+        RectTransform ContentWindowRect = ContentWindow.GetComponent<RectTransform>();
+        RectTransform ContentViewRect = ContentView.GetComponent<RectTransform>();
+        float max_height = 400f;
+        float cur_height = 45f;
+
+        // Set scroll view size by results
+        if (results.Count != 0)
+        {
+            ContentWindowRect.sizeDelta = new Vector2(ContentWindowRect.sizeDelta.x, cur_height * results.Count);
+            ContentViewRect.sizeDelta = new Vector2(ContentViewRect.sizeDelta.x, Mathf.Min(max_height, cur_height * results.Count));
+        }
+        else
+        {
+            ContentWindowRect.sizeDelta = new Vector2(ContentWindowRect.sizeDelta.x, 0f);
+            ContentViewRect.sizeDelta = new Vector2(ContentViewRect.sizeDelta.x, 0f);
+        }
+
+        // Instantiate result in scroll view
+        for (int resultIndex = 0; resultIndex < results.Count; resultIndex++)
+        {
+            Transform child = Instantiate(ItemPrefab, ItemParent);
+            child.GetComponentInChildren<Text>().text = results[resultIndex];
+            child.SetParent(ItemParent);
+            child.localPosition = new Vector2(0f, -cur_height * resultIndex);
+        }
+    }
+    private List<string> GetResults(string input)
+    {
+        // Get monster name by CSV Files.
+        List<Dictionary<string, object>> monster_names = CSVReader.Read("monster_name_merge");
+        List<string> mockData = new List<string>();
+        List<string> resultData = new List<string>();
+
+        for (int i = 0; i < monster_names.Count; i++)
+        {
+            mockData.Add(monster_names[i]["Name"].ToString());
+        }
+
+        // Get string that user write in field.
+        string keywords = input.ToLower().Replace(" ", "");
+
+        for (int idx = 0; idx< mockData.Count; idx++)
+        {
+            #region levenshtein logic
+            //int mock_distance = LevenKit.Extend.StringExtend.LevenshteinDistance(mockData[idx].ToLower().Replace(" ", ""), keywords, caseSensitive: false);
+            //levenshteinDistance = Mathf.Clamp01(levenshteinDistance);
+            //bool closeEnough = (int)(levenshteinDistance * mockData[idx].Length) > mock_distance;
+
+            //if (closeEnough)
+            //{
+            //    resultData.Add(mockData[idx]);
+            //    mockData.RemoveAt(idx);
+            //    idx--;
+            //}
+            #endregion
+
+            #region lcs logic
+            int lcs_value = CommonKit.Extend.StringCommon.LongestCommonSubsequence(mockData[idx].ToLower().Replace(" ", ""), keywords);
+            bool closeEnough = lcs_value >= keywords.Length;
+
+            if (closeEnough)
+            {
+                resultData.Add(mockData[idx]);
+                mockData.RemoveAt(idx);
+                idx--;
+            }
+            #endregion
+        }
+
+        return resultData;
+    }
+}
